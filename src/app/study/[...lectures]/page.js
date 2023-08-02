3246ea4af4e91456fdd40e25efc7ae3952d5351a
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-02 14:58:40 +0530

    segments of route completed

--- src/app/study/[...lectures]/page.js
+++ src/app/study/[...lectures]/page.js
@@ -3,8 +3,9 @@ const Lecture = ({ params }) => {
   console.log(params);
   return (
     <div>
-      <h1>{params.lectures[0]}</h1>
-      <h2>{params.lectures[1]}</h2>
+      <h1>Day of college {params.lectures[0]}</h1>
+      <h2>Lecture of {params.lectures[1]}</h2>
+      <p>completed Lectures no.: {params.lectures[2]}</p>
     </div>
   );
 };
