c764cc5c19a9690edf86efd2340cab4da6040b16
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-06 00:30:26 +0530

    conditionaly Styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import style from "./style.module.css";
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [name, setName] = useState("hello");
+
+  const [color, setColor] = useState("red");
+
+  const updateColor = () => {
+    if (color == "red") {
+      setColor("green");
+    } else {
+      setColor("red");
+    }
+  };
 
   const apple = () => {
     if (name === "hello") {
@@ -19,6 +30,9 @@ export default function Home() {
   const navigate = (name) => {
     router.push(name);
   };
+
+  const { red } = style;
+
   return (
     <main>
       <br />
@@ -41,6 +55,25 @@ export default function Home() {
       <br />
       <h1>Fetch Data with API in Server Components</h1>
       <Link href="/productListSSR">Go to Product List</Link>
+      <br />
+      <div>
+        <h1 className={color == "red" ? style.red : style.green}>
+          Conditional module Css
+        </h1>
+        <h2
+          style={{
+            backgroundColor: color == "red" ? "red" : "green",
+          }}
+        >
+          Background{" "}
+        </h2>
+        <h3 id={style.orange}>Id attribute Css Module</h3>
+        <h4 className={red}>Dummy Text</h4>
+        <h4 className={red}>Dummy Text</h4>
+        <h4 className={red}>Dummy Text</h4>
+        <h4 className={red}>Dummy Text</h4>
+        <button onClick={updateColor}>update Color</button>
+      </div>
     </main>
   );
 }
